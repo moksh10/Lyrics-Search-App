2e3273abadb92a64fbff890fccba29907cf640d6
MOKSH <mokshteng@gmail.com>
2021-07-14 14:37:53 +0530

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic } from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
+function Header() {
+    const icon = <FontAwesomeIcon icon={faMusic} />
+    return (
+        <>
+        <div className="logo">Lyrics Search</div>
+        <form className="search-section">
+            <div className="search-heading"><div className="icon">{icon}</div>Search for any song</div>
+            <div className="getAny">Get the lyrics for any song</div>
+            <input type="text" placeholder="Song title..." className="search-box" />
+            <button className="button">Get Track Lyrics</button>
+        </form>
+        </>
+    )
+}
+
+export default Header
