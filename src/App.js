e96d13a18dd83382fc7016d8a9a1e62fbe920522
MOKSH <mokshteng@gmail.com>
2021-07-18 12:20:22 +0530

    "DEPLOYEMENT"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import Home from './components/Home/Home'
 import Lyrics from './components/Lyrics/Lyrics';
-import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import { Switch,Route } from 'react-router-dom';
 export default function App() {
   
   return (
     
-    <BrowserRouter>
+    
     <Switch>
     <Route exact path='/'><Home /></Route>
     <Route path='/lyrics/:id'><Lyrics /></Route>
     </Switch>
-    </BrowserRouter>
     
     
     
